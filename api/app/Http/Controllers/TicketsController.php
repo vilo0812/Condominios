030eca69f6f8388da2f8450da627692d22ec6532
Cesar Montevideo <77177023+cesar899@users.noreply.github.com>
2022-09-28 11:02:06 -0400

    Update TicketsController.php

--- api/app/Http/Controllers/TicketsController.php
+++ api/app/Http/Controllers/TicketsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ticket;
+use Illuminate\Http\Request;
 use App\Models\MessageTicket;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
